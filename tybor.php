0bc7c0426ac1a9acafd8030e23205cb127da1195
Christian Nelson <zero@zerolives.org>
2009-04-19 23:13:24 -0400

    Little sanitization, addition of Apache version gathering, start of HTML template

--- tybor.php
+++ tybor.php
@@ -1,13 +1,18 @@
 <?php
 
+$message = "";
+$header = "";
+
 $validApps[] = "station identification";
 $validApps[] = "diggbarred";
 $validApps[] = "la-petite-url";
 
-$application = $_POST['app'];
-$version = $_POST['v'];
-$site = $_POST['id'];
-$extensions = $_POST['ext'];
+$application = strip_tags($_GET['app']);
+$version = strip_tags($_GET['v']);
+$site = strip_tags($_GET['id']);
+$extensions = stripslashes(strip_tags($_GET['ext']));
+$php_version = strip_tags($_GET['php']);
+$apache_version = strip_tags($_GET['apache']);
 
 if($site == "")
 {
@@ -26,15 +31,31 @@ if(in_array($application,$validApps))
 	fwrite($fp, '	');
 	fwrite($fp, $site);
 	fwrite($fp,'	');
+	fwrite($fp, $apache_version);
+	fwrite($fp, '	');
+	fwrite($fp, $php_version);
+	fwrite($fp, '	');
 	fwrite($fp, $extensions);
 	fwrite($fp, '
 ');
 	fclose($fp);
+	
+	$message = "Your plugin has been registered. Thanks! We're sending you back to your site, now.";
+	$header = '<meta http-equiv="refresh" content="5;URL='. $_GET['back_to'].'&registered=yes">';
 
 }
 else
 {
-	echo "not a valid application.";
+	$message =  "not a valid application.";
 }
 
 ?>
+<html>
+	<head>
+		<title>EXTRA FUTURE Plugin Registration</title>
+		<?php echo $header; ?>
+	</head>
+	<body>
+		<p><?php echo $message; ?></p>
+	</body>
+</html>
